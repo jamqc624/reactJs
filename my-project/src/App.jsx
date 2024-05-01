f43df47425b457c89220c6d8cfc96b58397cd701
jamqc624 <160290725+jamqc624@users.noreply.github.com>
2024-05-01 15:53:57 +0800

    ReactJS Laboratory
    commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='bg-red-00 inline-block w-32 h-32'></div>
+    <div className='bg-blue-400 inline-block w-32 h-32'></div>
+    <div className='bg-green-400 inline-block w-32 h-32'></div>
+    <div className='bg-yellow-400 inline-block w-32 h-32'></div>
+     
+    </>
+  )
+}
+
+export default App
